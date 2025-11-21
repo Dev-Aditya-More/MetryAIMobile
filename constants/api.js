be89300bd21620208babdc4c0cef730cce4722f0
Aditya More <aditya1875more@gmail.com>
2025-11-21 18:50:23 +0530

    just few rectifications

--- constants/api.js
+++ constants/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // axios.post(url,data)
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://unfetching-beulah-semiadhesive.ngrok-free.dev/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
