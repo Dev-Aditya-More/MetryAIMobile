ecaddf4de5b99b9287accce54903a30962c2e8a3
shahviraj222 <viraj22feb@gmail.com>
2025-11-21 11:54:31 +0530

    update

--- app/(onboarding)/reset-password.tsx
+++ app/(onboarding)/reset-password.tsx
@@ -13,7 +13,7 @@ import {
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { AuthService } from "../../utils/auth";
+import { AuthService } from "../../api/auth";
 
 export default function ResetPassword() {
   const [input, setInput] = useState({
